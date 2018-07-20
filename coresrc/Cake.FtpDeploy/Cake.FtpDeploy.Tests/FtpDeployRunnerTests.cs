f697427909c88bf99013bb6f67ef0dd4106da2bf
Christopher Pink <christopher.pink@extendhealth.com>
2018-07-20 10:22:31 -0600

    Add netstandard projects and solution

---
+++ coresrc/Cake.FtpDeploy/Cake.FtpDeploy.Tests/FtpDeployRunnerTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Cake.FtpDeploy.Tests
+{
+    public class FtpDeployRunnerTests
+    {
+
+        private readonly FtpDeployFixture<FtpDeploySettings> _fixture;
+
+        public FtpDeployRunnerTests()
+        {
+            _fixture = new FtpDeployFixture<FtpDeploySettings>();
+        }
+
+        [Fact]
+        public void RunsWithCorrectVariables()
+        {
+            _fixture.Settings.FtpUri = "ftp://www.test.com";
+            _fixture.Settings.FtpUserName = "testFtpUsername";
+            _fixture.Settings.FtpPassword = "testFtpPassword";
+            _fixture.Settings.ArtifactsPath = "./TestArtifacts";
+            var result = _fixture.Run();
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionsOnAllEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(() => _fixture.Run());
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionsOnUsernameEmpty()
+        {
+            _fixture.Settings.FtpUri = "ftp://www.test.com";
+            _fixture.Settings.FtpUserName = null;
+            _fixture.Settings.FtpPassword = "testFtpPassword";
+            _fixture.Settings.ArtifactsPath = "./TestArtifacts";
+            Assert.Throws<ArgumentNullException>(() => _fixture.Run());
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionsOnPasswordEmpty()
+        {
+            _fixture.Settings.FtpUri = "ftp://www.test.com";
+            _fixture.Settings.FtpUserName = "testFtpUsername";
+            _fixture.Settings.FtpPassword = null;
+            _fixture.Settings.ArtifactsPath = "./TestArtifacts";
+            Assert.Throws<ArgumentNullException>(() => _fixture.Run());
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionsOnArtifactsEmpty()
+        {
+            _fixture.Settings.FtpUri = "ftp://www.test.com";
+            _fixture.Settings.FtpUserName = "testFtpUsername";
+            _fixture.Settings.FtpPassword = "testFtpPassword";
+            _fixture.Settings.ArtifactsPath = null;
+            Assert.Throws<ArgumentException>(() => _fixture.Run());
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionsOnArtifactsBadDirectory()
+        {
+            _fixture.Settings.FtpUri = "ftp://www.test.com";
+            _fixture.Settings.FtpUserName = "testFtpUsername";
+            _fixture.Settings.FtpPassword = "testFtpPassword";
+            _fixture.Settings.ArtifactsPath = "some/non/existent/directory";
+            Assert.Throws<ArgumentException>(() => _fixture.Run());
+        }
+    }
+}
---
+++ coresrc/Cake.FtpDeploy/Cake.FtpDeploy/Services/WebService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cake.FtpDeploy.Services
+{
+    public class WebService : IWebService
+    {
+
+        private readonly Regex _splitRegex = new Regex("\\s+", RegexOptions.Compiled);
+
+        public void DeleteFile(string ftpUri, string username, string password)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(ftpUri);
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+            request.Credentials = new NetworkCredential(username, password);
+
+            var response = (FtpWebResponse)request.GetResponse();
+            response.Close();
+        }
+
+        public void RemoveDirectory(string ftpUri, string username, string password)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(ftpUri);
+            request.Method = WebRequestMethods.Ftp.RemoveDirectory;
+            request.Credentials = new NetworkCredential(username, password);
+
+            var response = (FtpWebResponse)request.GetResponse();
+            response.Close();
+        }
+
+        public void UploadFile(string ftpUri, string filePath, string username, string password)
+        {
+            using (var client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential(username, password);
+                client.UploadFile(ftpUri, "STOR", filePath);
+            }
+        }
+
+        public void MakeDirectory(string ftpUri, string username, string password)
+        {
+            var webRequest = (FtpWebRequest)WebRequest.Create(ftpUri);
+            webRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+            webRequest.Credentials = new NetworkCredential(username, password);
+
+            var response = (FtpWebResponse)webRequest.GetResponse();
+            response.Close();
+        }
+
+        public List<DirectoryDetails> ListDirectoryDetails(string ftpUri, string username, string password)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(ftpUri);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            request.Credentials = new NetworkCredential(username, password);
+
+            var directoryDetailsList = new List<DirectoryDetails>();
+            var response = (FtpWebResponse)request.GetResponse();
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var responseLine = streamReader.ReadLine();
+                    var split = _splitRegex.Split(responseLine);
+
+                    var directoryDetails = new DirectoryDetails()
+                    {
+                        Type = (DetermineIfDirectory(split)) ? DirectoryDetailsType.Directory : DirectoryDetailsType.File,
+                        Name = split.Last()
+                    };
+                    directoryDetailsList.Add(directoryDetails);
+                }
+            }
+            response.Close();
+
+            return directoryDetailsList;
+        }
+
+        private bool DetermineIfDirectory(string[] responseLineParts)
+        {
+            return (responseLineParts[0].StartsWith("d") || (responseLineParts.Length >= 3 && responseLineParts[2].ToLower().StartsWith("<d")));
+        }
+
+    }
+}
